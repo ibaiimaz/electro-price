0487e6aaab333ad73fa16d8bb92f125b3908e278
ibai imaz <ibaiimaz@gmail.com>
2023-11-17 09:57:05 +0100

    Lift up Heater condition

--- src/electro-price.ts
+++ src/electro-price.ts
@@ -111,57 +111,75 @@ export class ElectroPrice {
                     }
                 }
             }
-        } else {
-            if (season !== "autumn" && season !== "winter") {
-                if (season !== "spring") {
-                    if (this.device.name !== "Heater") {
-                        calculoFinal = calculoFinal + this.device.basePrice;
-                    }
-                }
-            } else {
-                if (this.device.name !== "Fan" && this.device.name !== "TV") {
-                    calculoFinal = calculoFinal + this.device.basePrice;
-                    if (this.device.name === "Heater") {
-                        calculoFinal = this.device.basePrice * 1.2;
-                    }
-                }
+        } else if (this.device.name === "Heater") {
+            if (season !== "autumn" && season !== "winter") {} else {
+                calculoFinal = calculoFinal + this.device.basePrice;
+                calculoFinal = this.device.basePrice * 1.2;
             }
 
             if (season !== "autumn") {
-                if (this.device.name !== "Heater") {
-                    if (season !== "summer" || this.device.name !== "Fan") {
-                        calculoFinal = this.device.basePrice;
-                    }
-                } else if (season === "spring") {
+                if (season === "spring") {
                     calculoFinal = this.device.basePrice;
                 }
                 if (season === "summer") {
                     calculoFinal = this.device.basePrice;
-                    if (this.device.name === "Heater") {
-                        calculoFinal = calculoFinal * 0.8;
-                    }
+                    calculoFinal = calculoFinal * 0.8;
                 }
             }
 
-            if (this.device.name === "TV" || this.device.name === "Heater" || this.device.name === "Fan") {
-                if (this.device.energyCertificate !== "A" && this.device.energyCertificate !== "C") {
-                    if (this.device.energyCertificate !== "B" && this.device.energyCertificate !== "D") {
-                        if (this.device.energyCertificate !== "E") {
-                            calculoFinal = calculoFinal + 10;
-                        } else {
-                            calculoFinal = calculoFinal + 30;
-                        }
-                    } else if (this.device.energyCertificate !== "B") {
-                        calculoFinal = calculoFinal + 50;
+            if (this.device.energyCertificate !== "A" && this.device.energyCertificate !== "C") {
+                if (this.device.energyCertificate !== "B" && this.device.energyCertificate !== "D") {
+                    if (this.device.energyCertificate !== "E") {
+                        calculoFinal = calculoFinal + 10;
                     } else {
-                        calculoFinal = calculoFinal + 80;
+                        calculoFinal = calculoFinal + 30;
                     }
+                } else if (this.device.energyCertificate !== "B") {
+                    calculoFinal = calculoFinal + 50;
                 } else {
-                    if (this.device.energyCertificate === "C") {
-                        calculoFinal = calculoFinal + 60;
-                    } else {
-                        calculoFinal = calculoFinal + 100;
+                    calculoFinal = calculoFinal + 80;
+                }
+            } else {
+                if (this.device.energyCertificate === "C") {
+                    calculoFinal = calculoFinal + 60;
+                } else {
+                    calculoFinal = calculoFinal + 100;
+                }
+            }
+
+            if ((this.device.weight >= 0) && (this.device.weight <= 199) && this.device.basePrice <= 1500) {
+                calculoFinal = calculoFinal + 10;
+            } else {
+                if (this.device.basePrice <= 1500) {
+                    if ((this.device.weight >= 200) && (this.device.weight <= 999)) {
+                        calculoFinal = calculoFinal + 50;
                     }
+                    if ((this.device.weight >= 1000) && (this.device.weight <= 4999)) {
+                        calculoFinal = calculoFinal + 80;
+                    } else {
+                        if ((this.device.weight >= 5000)) {
+                            calculoFinal = calculoFinal + 100;
+                        }
+                    }
+                }
+            }
+        } else {
+            if (season !== "autumn" && season !== "winter") {
+                if (season !== "spring") {
+                    calculoFinal = calculoFinal + this.device.basePrice;
+                }
+            } else {
+                if (this.device.name !== "Fan" && this.device.name !== "TV") {
+                    calculoFinal = calculoFinal + this.device.basePrice;
+                }
+            }
+
+            if (season !== "autumn") {
+                if (season !== "summer" || this.device.name !== "Fan") {
+                    calculoFinal = this.device.basePrice;
+                }
+                if (season === "summer") {
+                    calculoFinal = this.device.basePrice;
                 }
             }
 
